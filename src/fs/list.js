b81ae194ae940bab63f928d063e142f16050d82d
Marharyta Malets <marharyta_malets@epam.com>
2025-04-21 19:57:24 +0300

    feat: add list solution

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir, stat } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const fileFolderPath = join(__dirname, 'files');
+
+  await stat(fileFolderPath).catch(() => {
+    throw Error('FS operation failed');
+  });
+
+  const files = await readdir(fileFolderPath, {
+    withFileTypes: true
+  });
+
+  console.log(files.filter(file => file.isFile()).map(file => file.name));
 };
 
 await list();
