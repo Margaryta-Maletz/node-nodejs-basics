148cd2ea5072f883165f1fcd6b0c62d4534f64fe
Marharyta Malets <marharyta_malets@epam.com>
2025-04-21 21:11:54 +0300

    feat: add args solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argVars = process.argv;
+
+  for (let ind = 2; ind < argVars.length; ind +=2) {
+    console.log(`${argVars[ind].slice(2)} is ${argVars[ind+1]}`);
+  }
 };
 
 parseArgs();
