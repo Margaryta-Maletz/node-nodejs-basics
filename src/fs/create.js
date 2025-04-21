e1fcf0f8d1e724c57cfa281f6ac659299fd803ca
Marharyta Malets <marharyta_malets@epam.com>
2025-04-21 19:12:00 +0300

    feat: add create solution

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { stat, writeFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  await stat(filePath).then(() => {throw new Error('FS operation failed')}, (error) => {
+    if (error.code !== 'ENOENT') {
+      throw Error(error);
+    }
+  });
+
+  const content = 'I am fresh and young';
+  await writeFile(filePath, content);
 };
 
 await create();
