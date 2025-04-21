67e4ca629d9c8936e84a8ac3ebaf855d68c8e6af
Marharyta Malets <marharyta_malets@epam.com>
2025-04-21 20:00:24 +0300

    feat: add read solution

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile, stat } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+
+  await stat(filePath).catch(() => {
+    throw Error('FS operation failed');
+  });
+
+  const file = await readFile(filePath, { encoding: 'utf8' });
+  console.log(file);
 };
 
 await read();
