9de93fcdc4f8fbbbb222219c6fb3f9b1e5b3a4c5
Marharyta Malets <marharyta_malets@epam.com>
2025-04-21 19:28:40 +0300

    feat: add delete solution

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { stat, unlink } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
+  await stat(filePath).catch(() => {
+    throw Error('FS operation failed');
+  });
+
+  await unlink(filePath);
 };
 
 await remove();
