ab75b597ad17b87f47c93fe89cdf8bb2a72baf23
Marharyta Malets <marharyta_malets@epam.com>
2025-04-24 20:27:32 +0300

    feat: add decompress solution

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createUnzip } from "zlib";
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const archive = join(__dirname, 'files/archive.gz');
+  const writeFile = join(__dirname, 'files/fileToCompress.txt');
+
+  const readStream = fs.createReadStream(archive);
+  const writeStream = fs.createWriteStream(writeFile);
+
+  const unzip = createUnzip();
+
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
