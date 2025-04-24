4366d3736f484b798cb795c04ecc73380d51a064
Marharyta Malets <marharyta_malets@epam.com>
2025-04-24 19:53:45 +0300

    feat: add writeStream solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const fileName = join(__dirname, 'files/fileToWrite.txt');
+
+  const writeStream = fs.createWriteStream(fileName);
+
+  process.stdin.resume()
+    .setEncoding('utf-8')
+    .on('data', (chunk) => writeStream.write(chunk))
+    .on('end', () => writeStream.end());
+
+  console.log('Please input data. Press Enter. Press CTRL+C')
 };
 
 await write();
