0de30602a632aa36ac9784e102cc1f6289c102de
Marharyta Malets <marharyta_malets@epam.com>
2025-04-24 19:20:02 +0300

    feat: add calcHash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import crypto from 'crypto';
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileName = join(__dirname, 'files/fileToCalculateHashFor.txt');
+
+  const fileStream = fs.createReadStream(fileName);
+
+  const hash = crypto.createHash('sha256');
+
+  fileStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  fileStream.on('end', () => {
+    const resultHash = hash.digest('hex');
+    console.log(resultHash);
+  });
 };
 
 await calculateHash();
