b4163fa3bf373c3fb74d3b111e5e4c968f18f692
Marharyta Malets <marharyta_malets@epam.com>
2025-04-24 20:27:10 +0300

    feat: add compress solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from "zlib";
+import fs from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+  const readFile = join(__dirname, 'files/fileToCompress.txt');
+  const archive = join(__dirname, 'files/archive.gz');
+
+  const readStream = fs.createReadStream(readFile);
+  const writeStream = fs.createWriteStream(archive);
+
+  const zip = createGzip();
+
+  readStream.pipe(zip).pipe(writeStream);
 };
 
 await compress();
